2d464693b49168aa6af43724707e4280289828fd
rahukumar <34309815+rahukumar@users.noreply.github.com>
2022-11-09 12:27:54 +0530

    added input counter action

--- src/app/counter-input/counter-input.component.ts
+++ src/app/counter-input/counter-input.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addCounterValue } from '../state/counter.actions';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-input',
   templateUrl: './counter-input.component.html',
-  styleUrls: ['./counter-input.component.css']
+  styleUrls: ['./counter-input.component.css'],
 })
 export class CounterInputComponent implements OnInit {
+  inputValue: number;
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addCounter() {
+    this.store.dispatch(addCounterValue({ count: this.inputValue }));
   }
-
 }
--- src/app/state/counter.actions.ts
+++ src/app/state/counter.actions.ts
@@ -1,5 +1,11 @@
 import { createAction } from '@ngrx/store';
+import { props } from '@ngrx/store/src/action_creator';
 
 export const increment = createAction('increment');
 export const decrement = createAction('decrement');
 export const reset = createAction('reset');
+
+export const addCounterValue = createAction(
+  'addCounterValue',
+  props<{ count: number }>()
+);
